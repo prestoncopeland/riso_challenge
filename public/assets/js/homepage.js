59ea44e383d1352bb6c3d9418f188f1c4709ac99
Preston Copeland <copelandps1@gmail.com>
2016-08-16 16:49:26 -0400

    make registrant list sortable, add JQueryUI

--- public/assets/js/homepage.js
+++ public/assets/js/homepage.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
   $.getJSON("assets/js/test-users.json", function(data) {
-    var registrantList = []
-    var registrants = data.guests;
-    $.each( registrants, function(i, registrant) {
-        registrantList.push( "<li id='registrant-" + i + "'>" + registrant.name + "</li>" );
-      });
-
-    $("<ul/>", { "class": "registrant-list", html: registrantList}).appendTo( ".registrants" );
+    var registrantList = $("#registrant-list");
+    $(data.guests).each(function(index, registrant) {
+      registrantList.append($(document.createElement('li')).text(registrant.name));
+    });
   });
+  $("#registrant-list").sortable();
 });
