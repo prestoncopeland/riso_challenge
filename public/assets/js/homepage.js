4e32359079947a1a0a5e842a5b3a22185bb1e2b4
Preston Copeland <copelandps1@gmail.com>
2016-08-16 17:35:08 -0400

    begin to make registrant list items resemble mockup

--- public/assets/js/homepage.js
+++ public/assets/js/homepage.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $.getJSON("assets/js/test-users.json", function(data) {
     var registrantList = $("#registrant-list");
     $(data.guests).each(function(index, registrant) {
-      registrantList.append($(document.createElement('li')).text(registrant.name));
+      registrantList.append($(document.createElement('li')).text(registrant.name + " (" + registrant.email + ")").addClass("list-group-item"));
     });
   });
   $("#registrant-list").sortable();
