a11650665bd816275365b207d790b2e0f119423b
Preston Copeland <copelandps1@gmail.com>
2016-08-16 18:33:24 -0400

    add icon and formatting to registrant list

--- public/assets/js/homepage.js
+++ public/assets/js/homepage.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
   $.getJSON("assets/js/test-users.json", function(data) {
     var registrantList = $("#registrant-list");
-    $(data.guests).each(function(index, registrant) {
-      registrantList.append($(document.createElement('li')).text(registrant.name + " (" + registrant.email + ")").addClass("list-group-item"));
+
+    $(data.guests).each(function(index,registrant) {
+      registrantList.append($(document.createElement('li')).text(registrant.name + " (" + registrant.email + ")").addClass("list-group-item").prepend($(document.createElement('span')).addClass("glyphicon glyphicon-menu-hamburger")));
     });
   });
   $("#registrant-list").sortable();
