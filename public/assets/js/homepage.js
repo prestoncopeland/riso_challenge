239941c272415e95c44415bc85d4ff02cee68b38
Preston Copeland <copelandps1@gmail.com>
2016-08-16 23:38:23 -0400

    add x to delete registrant

--- public/assets/js/homepage.js
+++ public/assets/js/homepage.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     var registrantList = $("#registrant-list");
 
     $(data.guests).each(function(index,registrant) {
-      registrantList.append($(document.createElement('li')).text(registrant.name + " (" + registrant.email + ")").addClass("list-group-item").prepend($(document.createElement('span')).addClass("glyphicon glyphicon-menu-hamburger")));
+      registrantList.append($(document.createElement('li')).text(registrant.name + " (" + registrant.email + ")").addClass("list-group-item").prepend($(document.createElement('span')).addClass("glyphicon glyphicon-menu-hamburger")).append($(document.createElement('a')).addClass("close").attr("data-dismiss","alert").text("x")));
     });
   });
   $("#registrant-list").sortable();
