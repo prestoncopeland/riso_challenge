c65b19100eb230a515f23d5f6d1f514b9239c1de
Preston Copeland <copelandps1@gmail.com>
2016-08-16 23:53:04 -0400

    add data-target to close registrant list items

--- public/assets/js/homepage.js
+++ public/assets/js/homepage.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     var registrantList = $("#registrant-list");
 
     $(data.guests).each(function(index,registrant) {
-      registrantList.append($(document.createElement('li')).text(registrant.name + " (" + registrant.email + ")").addClass("list-group-item").prepend($(document.createElement('span')).addClass("glyphicon glyphicon-menu-hamburger")).append($(document.createElement('a')).addClass("close").attr("data-dismiss","alert").text("x")));
+      registrantList.append($(document.createElement('li')).text(registrant.name + " (" + registrant.email + ")").addClass("list-group-item").attr("id", "list-item-" + index).prepend($(document.createElement('span')).addClass("glyphicon glyphicon-menu-hamburger")).append($(document.createElement('a')).addClass("close").attr("data-dismiss","alert").attr("data-target", "list-item-" + index).text("x")));
     });
   });
   $("#registrant-list").sortable();
