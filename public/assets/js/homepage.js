a90e2233045694d095b775aa31f56911d8595ba4
Preston Copeland <copelandps1@gmail.com>
2016-08-17 03:20:01 -0400

    add xs

--- public/assets/js/homepage.js
+++ public/assets/js/homepage.js
@@ -3,8 +3,13 @@ $(document).ready(function() {
     var registrantList = $("#registrant-list");
 
     $(data.guests).each(function(index,registrant) {
-      registrantList.append($(document.createElement('li')).text(registrant.name + " (" + registrant.email + ")").addClass("list-group-item").attr("id", "list-item-" + index).prepend($(document.createElement('span')).addClass("glyphicon glyphicon-menu-hamburger")).append($(document.createElement('a')).addClass("close").attr("data-dismiss","alert").attr("data-target", "list-item-" + index).text("x")));
+      registrantList.append($(document.createElement('li')).text(registrant.name + " (" + registrant.email + ")").addClass("list-group-item").attr("id", "list-item-" + index).prepend($(document.createElement('span')).addClass("glyphicon glyphicon-menu-hamburger")).append($(document.createElement('a')).addClass("close").attr("data-dismiss","alert").attr("data-target",index).text("x")));
     });
   });
+
   $("#registrant-list").sortable();
+
+  $("a").clic‌​k(function(){
+    $(this).parents.find(".list-group-item").remove();
+  });
 });
